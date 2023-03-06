71d07c5ec0caf7e9c1b3b3c92f087b0e78df6ee0
unknown <moussahousseini@gmail.com>
2023-03-06 15:27:48 -0800

    commit 2

--- src/main/java/com/cenadi/demo/DemoApplication.java
+++ src/main/java/com/cenadi/demo/DemoApplication.java
@@ -2,10 +2,19 @@ package com.cenadi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    public static Logger logger=LoggerFactory.getLoger(SpringJenkinsApplication.class);
+
+    @PostConstruct
+	public void init(){
+		logger.info("Application started.....");
+	     
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/test/java/com/cenadi/demo/DemoApplicationTests.java
+++ src/test/java/com/cenadi/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package com.cenadi.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
+    public static Logger logger=LoggerFactory.getLoger(DemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test case execution");
+		assertEquals(true,true);
 	}
 
 }
